18907d75e2eb3813c1243daf85d81590561d673e
beucismis <beucismis@tutamail.com>
2021-03-21 17:01:47 +0300

    added docstrings

--- r2tg_bot/util.py
+++ r2tg_bot/util.py
@@ -7,17 +7,40 @@ import config as c
 
 
 def get_reddit():
-    return praw.Reddit("bot")
+    """Returns the Reddit client class.
+
+    Returns:
+        class: praw.Reddit
+    """
+    return praw.Reddit("r2tg_bot")
 
 
 def get_telegram():
+    """Return the Telegram client class.
+
+    Returns:
+        class: pyrogram.Client
+    """
     return pyrogram.Client("r2tg_bot", config_file="pyrogram.ini")
 
 
 def download_media(url, file_name):
+    """Download the Reddit media.
+
+    Parameters:
+        url (str): Media link
+        file_name (str): File name to save
+
+    Returns:
+        int: 0
+    """
     subprocess.run(
         [
-            f"ffmpeg -i {url}/DASHPlaylist.mpd -c copy {c.MEDIA_PATH}/{file_name}.mp4 2> /dev/null"
+            f"ffmpeg -i {0}/DASHPlaylist.mpd -c copy {1}/{2}.mp4 2> /dev/null".format(
+                url, c.MEDIA_PATH, file_name
+            )
         ],
         shell=True,
     )
+
+    return 0
