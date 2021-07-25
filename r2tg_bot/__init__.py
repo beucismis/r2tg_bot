7cb1c5de99b499794af7f1818e2243c20029d370
beucismis <beucismis@tutamail.com>
2021-07-25 17:07:04 +0300

    Update __init__.py

--- r2tg_bot/__init__.py
+++ r2tg_bot/__init__.py
@@ -19,34 +19,16 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-
-import click
-import subprocess
-
-import config as c
+import asyncio
+from bot import main
 
 
 __version__ = "0.6.3"
 __author__ = "Adil Gurbuz"
 __contact__ = "beucismis@tutamail.com"
 __source__ = "https://github.com/beucismis/r2tg_bot"
-__description__ = "Uploads Reddit videos to Telegram"
-
-
-@click.version_option(version=__version__)
-@click.option("--run", is_flag=True, help="Run the bot.")
-@click.option("--clearallmedia", is_flag=True, help="Clear all media and exit.")
-@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
-def main(run, clearallmedia):
-    if run:
-        from bot import run_the_bot
-
-        click.echo("Running...")
-        run_the_bot()
-
-    if clearallmedia:
-        subprocess.run([f"rm -rf {c.MEDIA_PATH}/*.mp4"], shell=True)
+__description__ = "A Reddit bot that uploads video or GIF files to Telegram"
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
